f801dfab72cbb489402a9ba349eb3e1fb3ecf3e4
deniskipeles <narydbest@gmail.com>
2024-01-08 13:29:38 +0300

    commit updates1

--- src/routes/api/ai/+server.ts
+++ src/routes/api/ai/+server.ts
@@ -5,10 +5,10 @@ import { PUBLIC_PALM_KEY } from '$env/static/public';
 import { pb } from '$lib/pocketbase';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
+
+/** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
   const searchParams = url.searchParams;
-
-  // Create an empty object to store the parameters
   const queryParams: Record<string, any> = {};
 
   // Loop through the search parameters and populate the object
@@ -21,74 +21,73 @@ export async function GET({ url }) {
       queryParams[key] = value;
     }
   });
+
   try {
-    const data: any = queryParams;
+    const data = queryParams;
     if (!data.key && !data.url) {
       return json({
         success: false,
-        error: 'Please provide the ai key from ktechs.\n Or obtain one to resolve this issue.'
+        error: 'Please provide the AI key from ktechs or obtain one to resolve this issue.',
       });
     }
 
     if (data.key || data.url) {
-      const record = await pb
-        .collection(data.type)
-        .getFirstListItem(`key="${data.key}"|| urls~"${data.url}"`, {})
-        .then(async (value) => value);
+      const record = await pb.collection(data.type).getFirstListItem(`key="${data.key}"|| urls~="${data.url}"`, {});
       if (!record) {
         return json({
           success: false,
-          error: 'Please register with ktechs as a client to obtain an ai key.'
+          error: 'Please register with ktechs as a client to obtain an AI key.',
         });
       }
-      if (
-        record &&
-        new Date(record?.ai_payment_expiry_date).getTime > new Date(Date.now()).getTime &&
-        record.number_of_ai_queries < 500
-      ) {
+
+      const aiPaymentExpiryDate = new Date(record?.ai_payment_expiry_date).getTime();
+      const currentDate = new Date().getTime();
+
+      if (record && aiPaymentExpiryDate > currentDate && record.number_of_ai_queries < 500) {
         return json({
           queryParams,
           success: true,
-          data: PUBLIC_PALM_KEY
+          data: PUBLIC_PALM_KEY,
         });
       } else {
         return json({
           queryParams,
           success: true,
-          data: 'Failed to prompted ai. \nPlease renew your ai subscription with ktechs.\n Or subscribe to one to resolve this issue.'
+          data: 'Failed to prompt AI.\nPlease renew your AI subscription with ktechs or subscribe to one to resolve this issue.',
         });
       }
     }
+
     if (data?.prompt) {
       const prompt = `${data.prompt}`;
-
       const res = await ai(prompt);
+
       if (!res) {
-        return json({ success: false, error: 'no response from ai try again.' });
+        return json({ success: false, error: 'No response from AI. Try again.' });
       }
+
       try {
-        if (typeof res == 'string' && res.length > 1) {
+        if (typeof res === 'string' && res.length > 1) {
           await pb.collection('ai_queries').create({
             url: data.url,
             prompt: data.prompt,
             server_url: data.server_url,
             request_id: data.request_id,
             type: data.type,
-            data: res
+            data: res,
           });
         }
-      } catch (error) {
-        // eslint-disable-next-line no-ex-assign
-        error = '';
+      } catch (_) {
+        // Ignore the error
       }
 
       return json({ success: true, data: res });
     }
-    return json({ success: false, error: 'no prompt provided' });
-  } catch (error: unknown) {
+
+    return json({ success: false, error: 'No prompt provided.' });
+  } catch (error) {
     return json({ success: false, error: serializeNonPOJOs(error) });
   }
-  return json({ queryParams });
 }
 
 const MODEL_NAME = 'gemini-pro';
