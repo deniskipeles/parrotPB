ea947c1f1ea4278d90bad6cb8bad971871273dfc
Kipeles Kemboi <47624821+deniskipeles@users.noreply.github.com>
2024-03-18 22:24:43 +0300

    Update links.ts

--- src/lib/pocketbase/logic/links.ts
+++ src/lib/pocketbase/logic/links.ts
@@ -34,11 +34,12 @@ export const create_sub_links = async (data: any) => {
 export const fetchLinks = async () => {
   try {
     // you can also fetch all records at once via getFullList
-    let records = await pb.collection('view_links').getFullList({
-      sort: '-created'
+    let records = await pb.collection('main_menu').getFullList({
+      		sort: '-created',
+		      expand: 'sub_menu_via_main_menu_id.sub_menu_list_via_sub_menu_id' //+relatedCollections?.join(','),
     });
     records = serializeNonPOJOs(records);
-    return groupByKey(records, 'link');
+    return records;
   } catch (error) {
     return { error: serializeNonPOJOs(error) };
   }
