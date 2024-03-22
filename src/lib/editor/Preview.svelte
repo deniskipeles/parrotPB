f424a24c392192b23d92a9d594badade2f4752d7
deniskipeles <narydbest@gmail.com>
2024-03-22 14:14:43 +0300

    Commit message

--- src/lib/editor/Preview.svelte
+++ src/lib/editor/Preview.svelte
@@ -38,7 +38,7 @@
     h4: "h4 text-lg mb-4",
     h5: "h5 text-base mb-4",
     h6: "h6 text-sm mb-4",
-    p: "p mb-4 flex-inline-svg",
+    p: "p mb-4",
     a: "a anchor text-blue-500 underline hover:no-underline",
     ul: "ul list-disc pl-6 mb-4",
     ol: "ol list list-decimal pl-6 mb-4",
@@ -85,14 +85,6 @@
     var: "var italic",
     // Add more elements and their corresponding Tailwind classes here
   };
-
-  function injectInlineSVGClass() {
-    const imgElements = document.querySelectorAll('p img');
-  
-    for (let i = 0; i < imgElements.length; i++) {
-      imgElements[i].classList.add('inline-svg');
-    }
-  }
   
   
 
@@ -101,8 +93,6 @@
   const change = () => {
     // Call the function to inject Tailwind classes into the HTML elements
     injectTailwindClasses(tailwindClasses);
-    // Call the function to inject the inline-svg class into img elements within p tags
-    injectInlineSVGClass();
     
     tableJsInjection()
     // CodeBlock Highlight
@@ -161,38 +151,23 @@
 </script>
 
 <!-- else content here 
-<article class="md prose lg:prose-xl max-w-full space-y-4 mb-2">
 -->
 <div
   class="flex w-full items-center justify-center rounded-md bg-white/5 p-[rfs(50px)] sm:p-5"
 >
-  <article
-    class="flex w-full max-w-full flex-1 flex-col gap-5 prose-headings:leading-tight prose-h1:text-50px prose-h2:text-40px prose-h3:text-30px prose-h4:text-25px prose-h5:text-20px prose-h6:text-18px prose-p:text-base prose-p:leading-relaxed prose-p:text-white/50 prose-a:text-base prose-a:text-main prose-a:underline prose-a:underline-offset-4 prose-blockquote:w-fit prose-blockquote:rounded-md prose-blockquote:border-l-2 prose-blockquote:border-l-white/50 prose-blockquote:bg-white/5 prose-blockquote:p-5 prose-strong:text-white prose-code:text-base prose-code:text-white/50 prose-ol:list-inside prose-ol:list-decimal prose-ol:space-y-2 prose-ol:text-base
+  <article class="md prose lg:prose-xl max-w-full space-y-4 mb-2">
+  <!--article class="flex w-full max-w-full flex-1 flex-col gap-5 prose-headings:leading-tight prose-h1:text-50px prose-h2:text-40px prose-h3:text-30px prose-h4:text-25px prose-h5:text-20px prose-h6:text-18px prose-p:text-base prose-p:leading-relaxed prose-p:text-white/50 prose-a:text-base prose-a:text-main prose-a:underline prose-a:underline-offset-4 prose-blockquote:w-fit prose-blockquote:rounded-md prose-blockquote:border-l-2 prose-blockquote:border-l-white/50 prose-blockquote:bg-white/5 prose-blockquote:p-5 prose-strong:text-white prose-code:text-base prose-code:text-white/50 prose-ol:list-inside prose-ol:list-decimal prose-ol:space-y-2 prose-ol:text-base
     prose-ul:list-inside prose-ul:list-disc prose-ul:space-y-2 prose-ul:text-base prose-img:w-auto prose-img:rounded-md"
-  >
+  >-->
     {@html marked.parse(markdown)}
   </article>
 </div>
 
 
 <style>
-  .inline-svg {
-    height: 1em; /* Adjust the height based on your font size */
-    width: auto; /* Maintain the aspect ratio of the SVG */
-    display: inline-block;
-    vertical-align: text-bottom; /* Align the bottom of the SVG with the text baseline */
-    margin: 0 0.25em; /* Add some margin around the SVG for better spacing */
+  h3 img,
+  p img,
+  li img {
+    vertical-align: middle;
   }
-  
-  .flex-inline-svg {
-    display: inline-flex;
-    align-items: center;
-  }
-  
-  .flex-inline-svg img {
-    height: 1em; /* Adjust the height based on your font size */
-    width: auto; /* Maintain the aspect ratio of the SVG */
-    margin: 0 0.25em; /* Add some margin around the SVG for better spacing */
-  }
-
 </style>
