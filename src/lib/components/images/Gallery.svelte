2263832f56159c4ffc78caf3dbe3815a1f6335f5
deniskipeles <narydbest@gmail.com>
2024-06-20 15:54:26 +0300

    update gallery

--- src/lib/components/images/Gallery.svelte
+++ src/lib/components/images/Gallery.svelte
@@ -4,11 +4,16 @@
 </script>
 
 <section class="grid grid-cols-2 md:grid-cols-4 gap-2">
-  {#each Array.from({ length: Math.ceil(images_.length / 3) }) as _, i}
+  {#each Array.from({ length: Math.ceil(images.length / 3) }) as _, i}
   <div class="grid gap-4">
-    {#each images_.slice(i * 3, (i + 1) * 3) as image}
+    {#each images.slice(i * 3, (i + 1) * 3) as image}
     <div>
-      <img class="h-auto max-w-full rounded-lg" src={image} alt="" />
+      <a 
+        href={image}
+        target="_blank"
+      >
+        <img class="h-auto max-w-full rounded-lg" src={`https://drogon-app.onrender.com/400x/${image}`} alt="" />
+      </a>
     </div>
     {/each}
   </div>
