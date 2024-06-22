15eb06165162d845bc38c10a0f68e1a58475eb42
deniskipeles <narydbest@gmail.com>
2024-06-22 07:38:44 +0300

    view article

--- src/routes/(app)/[main_link]/[sub_link]/[sub_menu_list_id]/+page.server.ts
+++ src/routes/(app)/[main_link]/[sub_link]/[sub_menu_list_id]/+page.server.ts
@@ -17,7 +17,7 @@ export const load = async ({ locals, url, params }) => {
     // fetch a paginated records list
     const perPage = Number(url.searchParams.get('perPage') ?? 30);
     const page = Number(url.searchParams.get('page') ?? 1);
-    const menu_filter = params?.sub_menu_list_id ?? url.searchParams.get('sub_menu_list_id') ?? ''
+    const menu_filter = params?.sub_menu_list_id || url.searchParams.get('sub_menu_list_id') || ''
     const filter = `sub_menu_list_id ?~ "${menu_filter}"`;
     const resultList = await pb.collection('view_articles_list').getList(Number(page), perPage, {
       filter,
