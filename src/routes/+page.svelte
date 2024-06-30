f4306de27e41689179394a0e0de3e455fd8914bb
deniskipeles <narydbest@gmail.com>
2024-06-30 11:17:00 +0300

    added new updates to support redis

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
 <script>
   //import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+  
+  const loadEruda = () => {
+		let script = document.createElement('script');
+    script.src = "//cdn.jsdelivr.net/npm/eruda"
+    document.head.append(script);
+		script.onload = () => {
+      window.eruda.init();
+		};
+	};
+	onMount(()=>{
+	  loadEruda()
+	  //loadPagepilot()
+	  //disableScrollHandling()
+	})
   export let data
   
   import ListArticles from '$lib/components/layouts/ListArticles.svelte';
